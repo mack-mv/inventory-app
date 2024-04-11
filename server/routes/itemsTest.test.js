a7368b7dadc7757ae5e3baa1fa3b8c732cfc01fe
Alexander <abeaumier@deloitte.com>
2024-04-11 15:06:46 -0700

    test for delete

--- server/routes/itemsTest.test.js
+++ server/routes/itemsTest.test.js
@@ -2,8 +2,10 @@ const request = require('supertest');
 const express = require('express');
 const router = require('../routes/items'); 
 const app = express();
+
 app.use(express.json());
 app.use('/api/items', router);
+
 // Alex - create tests for items (npm run test, in terminal to run tests)
 describe('Item API', () => {
     it('should GET all items', async () => {
@@ -27,4 +29,19 @@ describe('Item API', () => {
         .expect(200);
       expect(res.body.name).toEqual('test item');
     });
+
+    describe('DELETE /api/items/:id', () => {
+      it('should respond with status code 204 when item is successfully deleted', async () => {
+        const itemId = 48; // replace with an id of an existing item
+        const response = await request(app).delete(`/api/items/${itemId}`);
+        expect(response.statusCode).toBe(204);
+      });
+    
+      it('should respond with status code 500 when item does not exist', async () => {
+        const itemId = 9999; // replace with an id of a non-existing item
+        const response = await request(app).delete(`/api/items/${itemId}`);
+        expect(response.statusCode).toBe(500);
+      });
+    });
+  
   });
